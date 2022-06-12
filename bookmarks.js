e5c9c0e1683c32e3ddd1dfaf7e10bcb317790d7e
Johan Rodríguez <johan@dell.localhost>
2022-06-12 04:07:51 -0600

    First config

---
+++ bookmarks.js
@@ -0,0 +1,34 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Gmail", url: "https://mail.google.com/mail/u/0/#inbox" },
+      { name: "Outlook", url: "https://outlook.office.com/mail/" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Notion", url: "https://www.notion.so/" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Music", url: "https://music.youtube.com/" },
+      { name: "Mastermind", url: "https://www.mastermind.ac/" },
+    ],
+  },
+  {
+    title: "Tools",
+    links: [
+      { name: "Translator", url: "https://www.ingles.com/" },
+      { name: "Colab", url: "https://colab.research.google.com/" },
+      { name: "Online code", url: "https://www.programiz.com/python-programming/online-compiler/" },
+    ],
+  },
+  {
+    title: "TEC",
+    links: [
+      { name: "Digital", url: "https://tecdigital.tec.ac.cr/dotlrn/index" },
+    ],
+  },
+];
